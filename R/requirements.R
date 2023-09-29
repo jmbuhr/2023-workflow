a17aca65fa2758f5d15ba9105175b9f7db9794c9
Jannik Buhr <jannik.m.buhr@gmail.com>
2023-09-29 02:31:54 +0200

    requirements

---
+++ R/requirements.R
@@ -0,0 +1,8 @@
+install.packages("tidyverse")
+install.packages("targets")
+install.packages("glue")
+install.packages("countdown")
+install.packages("DT")
+install.packages("ggbeeswarm")
+install.packages("palmerpenguins")
+
