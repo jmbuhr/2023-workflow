3e03ffb149b22d808265725de48aa2f200c952a7
Jannik Buhr <jannik.m.buhr@gmail.com>
2023-11-22 16:06:15 +0100

    update

--- _targets.R
+++ _targets.R
@@ -11,11 +11,8 @@ clean_data <- function(data) {
     rename_with(~ str_replace(.x, "_mm", "_cm"), ends_with("_mm"))
 }
 
-# Set target-specific options such as packages:
-# tar_option_set(packages = "utils") # nolint
-
 # End this file with a list of target objects.
 list(
   tar_target(penguins, palmerpenguins::penguins),
-  tar_target(clean_penguins, clean_data(penguins))
+  tar_target(clean_penguins, clean_data(clean_penguins))
 )
